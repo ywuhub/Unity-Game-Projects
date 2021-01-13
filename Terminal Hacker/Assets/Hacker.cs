1886e55629681877c07c17ca1717d4d165f298ae
Allan Wu <allan.wu6@gmail.com>
2021-01-13 22:34:24 +1100

    Terminal Hacker Game
    Menu Added

---
+++ Terminal Hacker/Assets/Hacker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terminal.WriteLine("Welcome to my Anagram Solver!\n");
+        Terminal.WriteLine("Select Game Difficulty:");
+        Terminal.WriteLine("Press 1 for Easy.");
+        Terminal.WriteLine("Press 2 for Medium.");
+        Terminal.WriteLine("Press 3 for Hard.\n");
+        Terminal.WriteLine("Enter your selection:");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
